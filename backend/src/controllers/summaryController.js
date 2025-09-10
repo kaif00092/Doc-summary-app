e9cbbb58f1024beaa96eaa8994b9f6e135d041aa
kaif00092 <kaifarshad07@gmail.com>
2025-09-10 12:16:17 +0530

    commititng

--- backend/src/controllers/summaryController.js
+++ backend/src/controllers/summaryController.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { createWorker } from "tesseract.js";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 export const generateSummary = async (req, res) => {
   console.log("--- STEP 1: Request received on backend. ---");
